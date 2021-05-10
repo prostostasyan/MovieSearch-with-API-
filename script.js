6d19a206814bbc0d7708a587f2abbc60010fe3c2
prostostasyan <67874612+prostostasyan@users.noreply.github.com>
2021-05-10 15:03:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let films = (obj) => {
     let count = 0;
     arrSearch = obj.results;
     console.log(obj);
-    if(arrSearch.length != 0)
+    if (arrSearch.length != 0)
         for (let value of obj.results) {
             let imageSrc = null;
             if (value.poster_path !== null && (value.poster_path !== undefined)) {
@@ -64,16 +64,16 @@ let films = (obj) => {
             newDiv.append(popup);
             main.append(newDiv);
         }
-        else {
-            main.innerHTML = `<div class="nothingFind"> Извините!<br> Такого здесь нет!</div>`
-        }
+    else {
+        main.innerHTML = `<div class="nothingFind"> Извините!<br> Такого здесь нет!</div>`
+    }
 }
 
 let sent = (event) => {
     event.preventDefault();
 
     let textInput = document.getElementById('searchInput').value;
-    if(textInput.length != 0){
+    if (textInput.length != 0) {
         main.innerHTML = '';
         request(urlSearch + textInput);
     }
@@ -83,8 +83,8 @@ let request = (url) => {
     // delay(1000)
     //     .then(() => fetch(url))
     fetch(url)
-        .then((response)=>{
-            if (response.status !== 200){
+        .then((response) => {
+            if (response.status !== 200) {
                 return Promise.reject(value);
             }
             return response.json();
@@ -96,8 +96,8 @@ if (arrSearch == null) {
     document.addEventListener('DOMContentLoaded', () => {
 
         fetch('https://api.themoviedb.org/3/trending/all/week?api_key=b8af74b0a4e83b8c71f1deb2c4a36ca9&language=ru')
-            .then((response)=>{
-                if (response.status !== 200){
+            .then((response) => {
+                if (response.status !== 200) {
                     return Promise.reject(value);
                 }
                 return response.json();
@@ -119,58 +119,71 @@ document.getElementById('title').onclick = function () {
 
 main.onclick = (event) => {
     if (event.target.className == 'moreDetails') {
+        let imageSrc;
         let numCell = event.target.getAttribute('data-id');
         let idMovie = event.target.getAttribute('data-globalid');
         let type = event.target.getAttribute('data-type');
-
-            let imageSrc = 'https://image.tmdb.org/t/p/original' + arrSearch[numCell].poster_path;
-            let popup = event.target.parentElement.parentElement.querySelector('.popup');
-
-            popup.style.display = 'block';
-            document.getElementById("overlay").style.display = "block";
-            popup.innerHTML = '';
+        if(arrSearch[numCell].poster_path != null){
+            imageSrc = 'https://image.tmdb.org/t/p/original' + arrSearch[numCell].poster_path;
+        }else {
+            imageSrc = 'img/no_poster.jpg';
+        }
 
 
-            let butPopup = document.createElement("button");
-            let textPopup = document.createElement("div");
+        let popup = event.target.parentElement.parentElement.querySelector('.popup');
+
+        popup.style.display = 'block';
+        document.getElementById("overlay").style.display = "block";
+        popup.innerHTML = '';
+
+
+        let butPopup = document.createElement("button");
+        let textPopup = document.createElement("div");
 
 
         let video = document.createElement('div');
         video.classList.add('video');
         fetch(`https://api.themoviedb.org/3/${type}/${idMovie}/videos?api_key=${keyId}&language=ru`)
             .then(response => response.json())
-            .then(data => `https://www.${data.results[0].site}.com/embed/${data.results[0].key}`)
+            .then(data => {
+                if ( data.results.length !== 0){
+                    // console.log(data);
+                    return `https://www.${data.results[0].site}.com/embed/${data.results[0].key}`
+                }else {
+                    return 'img/noVideo.jpg'}
+            })
             .then(videoSite => video.innerHTML = `<iframe src=${videoSite} frameborder="0" allowfullscreen>`)
 
-            let infoMovie = document.createElement("div");
-            infoMovie.classList.add('infoMovie')
-            let release_date = arrSearch[numCell].release_date;
-            if (release_date == undefined) {
-                release_date = "Нам пока неизвестна"
-            }
-            const vote_average = arrSearch[numCell].vote_average;
-            infoMovie.innerHTML = `<div> Дата релиза: ${release_date} </div> <div>Оценка пользователей: ${vote_average}</div>`
+        let infoMovie = document.createElement("div");
+        infoMovie.classList.add('infoMovie')
+        let release_date = arrSearch[numCell].release_date;
+        if (release_date == undefined) {
+            release_date = "Нам пока неизвестна"
+        }
+        const vote_average = arrSearch[numCell].vote_average;
+        infoMovie.innerHTML = `<div> Дата релиза: ${release_date} </div> <div>Оценка пользователей: ${vote_average}</div>`
 
-            textPopup.classList.add('textPopup');
-            butPopup.innerHTML = 'X';
-            butPopup.classList.add('btn');
+        textPopup.classList.add('textPopup');
+        butPopup.innerHTML = 'X';
+        butPopup.classList.add('btn');
 
-            let newImg = document.createElement("img");
-            newImg.setAttribute('src', imageSrc);
-            newImg.setAttribute('alt', arrSearch[numCell].title);
-            newImg.classList.add('mini-poster');
+        let newImg = document.createElement("img");
+        newImg.setAttribute('src', imageSrc);
+        newImg.setAttribute('alt', arrSearch[numCell].title);
 
-            if (arrSearch[numCell].overview !== '') {
-                textPopup.innerHTML = `<div class = 'descript'>${arrSearch[numCell].overview}</div>`;
-            } else {
-                textPopup.innerHTML = `<div class = 'descript'>Описание не найдено</div>`;
-            }
+        newImg.classList.add('mini-poster');
 
-            popup.append(newImg);
-            popup.append(butPopup);
-            popup.append(video);
-            textPopup.prepend(infoMovie);
-            popup.append(textPopup);
+        if (arrSearch[numCell].overview !== '') {
+            textPopup.innerHTML = `<div class = 'descript'>${arrSearch[numCell].overview}</div>`;
+        } else {
+            textPopup.innerHTML = `<div class = 'descript'>Описание не найдено</div>`;
+        }
+
+        popup.append(newImg);
+        popup.append(butPopup);
+        popup.append(video);
+        textPopup.prepend(infoMovie);
+        popup.append(textPopup);
     }
     if (event.target.className === 'btn') {
         let popup = event.target.parentElement;
